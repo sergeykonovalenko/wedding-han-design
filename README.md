94b6458300110065b4d7b58718fcaa133ca0f303
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2019-05-13 22:32:26 +0300

    Завершил верстку оффера

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Wedding HanDesign
-Оффициальный сайт английской компании «Wedding HanDesign» по продаже свадебных аксессуаров для планирования и проведения свадьбы
+Официальный сайт английской компании «Wedding HanDesign» по продаже свадебных аксессуаров для планирования и проведения свадьбы
 [http://han-design-wedding.sergeykonovalenko.com.ua](http://han-design-wedding.sergeykonovalenko.com.ua)
--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
+    'use strict';
 
+    const is_mobile = isMobile();
+
+    if (is_mobile) {
+        document.querySelector("body").classList.add('is-mobile');
+    }
+
+    let scene = document.getElementById('scene-1');
+    let parallaxInstance = new Parallax(scene);
+
+    let scene2 = document.getElementById('scene-2');
+    let parallaxInstance2 = new Parallax(scene2);
+
+    function isMobile() {
+        return $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+    }
+
+    function numberWithSpaces(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+    }
 }); // end ready
