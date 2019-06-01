7866c56b0931e0b7a9c077a288a5ef38490670aa
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2019-06-01 11:08:59 +0300

    Завершил верстку блока ***Get in touch***

--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -28,6 +28,87 @@ $(document).ready(function () {
         pauseOnHover: false
     });
 
+    // masked input
+    $('input[type="tel"]').mask('+44 (0) 99-9999-99-99');
+
+
+    ////////////////////////////////////////////////////////////////////////////
+    // FORM PROCESSING
+
+    $('[data-submit]').on('click', function(e) {
+        e.preventDefault();
+        $(this).parent('form').submit();
+    });
+
+    $.validator.addMethod(
+        "regex",
+        function(value, element, regexp) {
+            let re = new RegExp(regexp);
+            return this.optional(element) || re.test(value);
+        },
+        "Please check your details"
+    );
+
+    function valEl(el) {
+        let validator = el.validate({
+            rules:{
+                name:{
+                    required:true
+                },
+                email:{
+                    required:true
+                },
+                phone:{
+                    required:true,
+                    regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+                }
+            },
+            messages:{
+                name:{
+                    required:'Required field'
+                },
+                email:{
+                    required:'Required field',
+                    email:'Wrong email format'
+                },
+                phone:{
+                    required:'Required field',
+                    regex:'Wrong phone format'
+                }
+            },
+            submitHandler: function (form) {
+                $('.modal-order').modal('hide');
+                $('.loader').fadeIn();
+                let $form = $(form);
+                let $formId = $(form).attr('data-id');
+
+                $.ajax({
+                    type: 'POST',
+                    url: $form.attr('action'),
+                    data: $form.serialize(),
+                })
+                    .always(function (response) {
+                        setTimeout(function () {
+                            $('.loader').fadeOut();
+                        },800);
+                        setTimeout(function () {
+                            $('.modal-thanks').modal('show');
+                            $( 'input:not([type="hidden"]), textarea' ).val('');
+                            $('.form-extra__item').removeClass('form-extra__item--should-float');
+                        },1100);
+                    });
+
+                return false;
+            }
+        });
+    }
+
+    $('.js-form').each(function() {
+        valEl( $(this) );
+    });
+
+    ////////////////////////////////////////////////////////////////////////////
+
     function isMobile() {
         return $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
     }
